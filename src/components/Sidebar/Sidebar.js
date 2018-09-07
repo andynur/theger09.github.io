dd0ab8bd185e622b10551df9488376f739d461c8
Saipul Hidayat <dev.theger09@gmail.com>
2018-09-07 07:54:17 +0700

    set picture in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import ProfilePicture from '../../assets/img/profile.png'
 
 class Sidebar extends Component {
     render() { 
         return (
             <div className="sidebar">
-                <div></div>
+                <div className="picture-profile">
+                    <img src={ProfilePicture} alt="Profile Picture" srcset={ProfilePicture}/>
+                </div>
             </div>
         );
     }
