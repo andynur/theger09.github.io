4e68791461f9da14aae597f8d55b4b3cdba55be4
Saipul Hidayat <dev.theger09@gmail.com>
2018-09-07 07:53:37 +0700

    change import style css folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/css/style.css'
+import './assets/css/style.css'
 import 'pe7-icon/dist/dist/pe-icon-7-stroke.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
